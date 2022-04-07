7cbecd62fd7509a24dc8acf52353df389770737f
Martynov Evgeny <martynov@wimix.by>
2022-04-07 19:18:55 +0300

    project and freamworks added

---
+++ testProj/testProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testProj
+//
+//  Created by Martynov Evgeny on 7.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
